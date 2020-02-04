7dd3b5fce1c012fe0a708ecb90cdf949bc833a03
Philipp Storz <philipp.storz@bareos.com>
2020-02-04 10:01:18 +0100

    get lmdb also from third-party

--- core/src/filed/accurate.h
+++ core/src/filed/accurate.h
@@ -2,7 +2,7 @@
    BAREOS® - Backup Archiving REcovery Open Sourced
 
    Copyright (C) 2013-2014 Planets Communications B.V.
-   Copyright (C) 2013-2018 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2020 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -142,7 +142,7 @@ class BareosAccurateFilelistHtable : public BareosAccurateFilelist {
 
 #ifdef HAVE_LMDB
 
-#include "lmdb/lmdb.h"
+#include "lmdb/libraries/liblmdb/lmdb.h"
 
 /*
  * Lightning Memory DataBase (LMDB) specific storage abstraction class using the
